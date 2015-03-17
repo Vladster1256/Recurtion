273b1d466960aa899c87bc5dcb328bd8830ba34d
Vladster1256 <vcmariorocks@gmail.com>
2015-03-17 13:43:20 -0600

    Added to repo, does fibonachi counter
    woot m8

---
+++ src/main/controller/RecurtionController.java
@@ -0,0 +1,42 @@
+package main.controller;
+
+public class RecurtionController
+{
+	public void start()
+	{
+//		for(int spot = 0; spot < 100; spot++)
+//		{
+//			long startTime = System.currentTimeMillis();
+//			System.out.println("The " + (spot+1) + "'th position of the Fibonacci sequence is: "+ getFibonacci(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
+//		}
+		
+		for(int spot = 0; spot < 200; spot++)
+		{
+			long startTime = System.currentTimeMillis();
+			System.out.println("The " + (spot+1) + "'th position of the Factorial sequence is: "+ getFactorial(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
+		}
+	}
+	private double getFibonacci(double position)
+	{
+		if(position == 0 || position == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return getFibonacci(position - 2) + getFibonacci(position - 1);
+		}
+	}
+	
+	private double getFactorial(double value)
+	{
+		if(value == 0 || value == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return value * getFactorial(value -1);
+		}
+	}
+}
---
+++ src/main/controller/RecurtionRunner.java
@@ -0,0 +1,11 @@
+package main.controller;
+
+public class RecurtionRunner
+{
+	public static void main(String [] args)
+	{
+		RecurtionController controller = new RecurtionController();
+		controller.start();
+	}
+	
+}
