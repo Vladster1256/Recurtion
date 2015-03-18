f2956d556a8719cc7a135e8b75de9fe83432ae14
Vladster1256 <vcmariorocks@gmail.com>
2015-03-18 11:41:28 -0600

    uwot m8
    kk rek kill pepperoni

--- src/main/controller/RecurtionController.java
+++ src/main/controller/RecurtionController.java
@@ -4,17 +4,17 @@ public class RecurtionController
 {
 	public void start()
 	{
-//		for(int spot = 0; spot < 100; spot++)
-//		{
-//			long startTime = System.currentTimeMillis();
-//			System.out.println("The " + (spot+1) + "'th position of the Fibonacci sequence is: "+ getFibonacci(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
-//		}
-		
-		for(int spot = 0; spot < 200; spot++)
+		for(int spot = 0; spot < 100; spot++)
 		{
 			long startTime = System.currentTimeMillis();
-			System.out.println("The " + (spot+1) + "'th position of the Factorial sequence is: "+ getFactorial(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
+			System.out.println("The " + (spot+1) + "'th position of the Fibonacci sequence is: "+ getFibonacci(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
 		}
+		
+//		for(int spot = 0; spot < 200; spot++)
+//		{
+//			long startTime = System.currentTimeMillis();
+//			System.out.println("The " + (spot+1) + "'th position of the Factorial sequence is: "+ getFactorial(spot) + ". It took " + (System.currentTimeMillis()-startTime)/1000 + " seconds to calculate it.");
+//		}
 	}
 	private double getFibonacci(double position)
 	{
